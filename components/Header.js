81a4b8f583e63f8f12fa1e37be42e28474e29b3a
Bruno Gianini <bruno.gianini@driven.cx>
2022-07-31 21:53:35 -0300

    boostrap

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,6 @@ export default class Header extends Component {
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>Vaga FullStack Desafio</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"></link> 
         
       </Head>
     )
