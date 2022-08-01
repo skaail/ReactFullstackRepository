5e58339fdf3ef021e2ed7816497cabedc589b0e3
Bruno Gianini <bruno.gianini@driven.cx>
2022-07-31 21:33:09 -0300

    commit change table key

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,7 @@ export default class Header extends Component {
     return (
         <head>
             <title>Vaga FullStack Desafio</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"></link>
-            <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></Script>
-        </head>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"></link>        </head>
         
     )
   }
