caba4558dea41c94d8262536bb52f16ffc7198d3
Bruno Gianini <bruno.gianini@driven.cx>
2022-07-31 23:55:19 -0300

    teste fetch

--- pages/historico_vendas/index.js
+++ pages/historico_vendas/index.js
@@ -2,26 +2,10 @@ import { createClient } from "next-sanity";
 import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
 import Table from 'react-bootstrap/Table';
-import React, { useEffect } from "react";
 
 
 export default function IndexPage({ vendas }) {
-
-  useEffect(() => {
-    let headersList = {
-      "Accept": "*/*",
-      "Authorization": "Bearer skzhPshmupTGmkSdXPlS7FpNiRMO3tUqa3LLNNLOGtyTQZGNPcdbpapns53y6Awm63SM89wrzJro3WVmGB6dbxQsNLctMx6bEJ78eau0ZVu4TrhaHGsDEfiShpuycmSV5EKcwHg6g4nf2JHuUowU4evZ13pBJcbaez4pvNTvyccig3yrKO9H"
-     }
-     
-     fetch("https://tmdmvjqt.api.sanity.io/v2021-10-21/data/query/compras?query=*%5B_type%20%3D%3D%20%22vendas%22%5D", { 
-       method: "GET",
-       headers: headersList
-     }).then(function(response) {
-       return response.text();
-     }).then(function(data) {
-       console.log(data);
-     })
-   }, []);
+  
 
   return (
     <>
@@ -65,13 +49,11 @@ const client = createClient({
 });
 
 
-export async function getStaticProps() {
-  const vendas = await client.fetch(`*[_type == "vendas"]`);
-  return {
-    props: {
-      vendas
-    }
-  };
 
+const query = '*[_type == "vendas"]'
 
-}
+const subscription = client.listen(query)
+  .subscribe(update => {
+    const vendas = update.result
+    console.log(`${vendas.cliente} commented: ${comment.produto}`)
+  })
