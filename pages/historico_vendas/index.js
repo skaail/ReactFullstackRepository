adbf9627141505611371e6d0563febf1bd45e03d
Bruno Gianini <bruno.gianini@driven.cx>
2022-07-31 23:47:52 -0300

    user

--- pages/historico_vendas/index.js
+++ pages/historico_vendas/index.js
@@ -6,6 +6,22 @@ import Table from 'react-bootstrap/Table';
 
 export default function IndexPage({ vendas }) {
 
+  useEffect(() => {
+    let headersList = {
+      "Accept": "*/*",
+      "Authorization": "Bearer skzhPshmupTGmkSdXPlS7FpNiRMO3tUqa3LLNNLOGtyTQZGNPcdbpapns53y6Awm63SM89wrzJro3WVmGB6dbxQsNLctMx6bEJ78eau0ZVu4TrhaHGsDEfiShpuycmSV5EKcwHg6g4nf2JHuUowU4evZ13pBJcbaez4pvNTvyccig3yrKO9H"
+     }
+     
+     fetch("https://tmdmvjqt.api.sanity.io/v2021-10-21/data/query/compras?query=*%5B_type%20%3D%3D%20%22vendas%22%5D", { 
+       method: "GET",
+       headers: headersList
+     }).then(function(response) {
+       return response.text();
+     }).then(function(data) {
+       console.log(data);
+     })
+   }, []);
+
   return (
     <>
         <Header></Header>
