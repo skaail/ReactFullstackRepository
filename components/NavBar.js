faccc1270c574b8c0f0651962adfd3fdd6bf74db
Bruno Gianini <bruno.gianini@driven.cx>
2022-07-31 21:30:47 -0300

    4

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,20 +5,28 @@ export default class NavBar extends Component {
   render() {
     return (
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="/">Gerenciador de Vendas</a>
+    <Link href="/">
+      <a className="navbar-brand">Gerenciador de Vendas</a>
+    </Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item active">
-        <a className="nav-link" href="/historico_vendas">Histórico de vendas <span className="sr-only"></span></a>
+        <Link href="/historico_vendas">
+          <a className="nav-item active">Histórico de vendas</a>
+        </Link>
       </li>
       <li className="nav-item active">
-        <a className="nav-link" href="/cadastro_vendas">Cadastro de vendas <span className="sr-only"></span></a>
+        <Link href="/cadastro_vendas">
+            <a className="nav-item active">Cadastro de vendas</a>
+        </Link>
       </li>
       <li className="nav-item active">
-        <a className="nav-link" href="#">Cadstro de Vendedor <span className="sr-only"></span></a>
+        <Link href="#">
+            <a className="nav-item active">Cadstro de Vendedor</a>
+        </Link> 
       </li>
     </ul>
   </div>
