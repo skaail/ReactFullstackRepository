b2c4c6966fc0e24415c5638f5167671a23328286
skaailpvp <skaailpvp@gmail.com>
2022-08-03 08:12:12 -0300

    index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
     import { createClient } from "next-sanity";
     import Header from "../components/Header";
     import NavBar from "../components/NavBar";
@@ -6,20 +5,10 @@
     import {app} from '../firebaseConfig'
     import { useRouter } from "next/router";
     import {collection, addDoc} from 'firebase/firestore'
-=======
-import { createClient } from "next-sanity";
-import Header from "../components/Header";
-import NavBar from "../components/NavBar";
-import { useEffect } from 'react'
-import {app} from '../firebaseConfig'
-import { useRouter } from "next/router";
-import {collection, addDoc} from 'firebase/firestore'
->>>>>>> 4ea7031817f63433f3276a25b1286fcc12c0d358
 
 
 
 
-<<<<<<< HEAD
     export default function IndexPage({ vendas }) {
     let router = useRouter()
 
@@ -29,22 +18,10 @@ import {collection, addDoc} from 'firebase/firestore'
             router.push('/home')
         
     }, [])  
-=======
-export default function IndexPage({ vendas }) {
-  let router = useRouter()
-
-  useEffect(() => {
-  
-
-        router.push('/home')
-    
-  }, [])  
->>>>>>> 4ea7031817f63433f3276a25b1286fcc12c0d358
 
 
 
 
-<<<<<<< HEAD
     return (
         <>
         <header>
@@ -54,16 +31,3 @@ export default function IndexPage({ vendas }) {
         </>
     );
     }
-=======
-  return (
-    <>
-        <Header></Header>
-        <NavBar></NavBar>
-      <header>
-        <h1>Carregando...</h1>
-      </header>
-
-    </>
-  );
-}
->>>>>>> 4ea7031817f63433f3276a25b1286fcc12c0d358
